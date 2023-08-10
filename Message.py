fee3308b5ddf38ef5d1efa167781c04aefc25c81
조연수 <cys@innogrid.com>
2023-08-10 10:52:33 +0900

    리팩토링 ( 메세지 받는 함수 추가 )

---
+++ Message.py
@@ -0,0 +1,17 @@
+from Validation import checkIP
+
+
+def getMessage(host, community, message):
+    if host == '' or community == '':
+        if host == '' and community != '':
+            print('No IP')
+            message.append('IP 주소 없음')
+        elif host != '' and community == '':
+            print('No Community')
+            message.append('커뮤니티명 없음')
+        else:
+            print('No IP & Community')
+            message.append('IP 주소와 커뮤니티명 모두 없음')
+    elif not checkIP(host):
+        print('Not available IP')
+        message.append('IP 주소 올바르지 않음')
